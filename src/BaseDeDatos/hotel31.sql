451e0150efd8439bffb15d658a67c1cc4f02749d
Garaf <Mr.Churches@gmail.com>
2023-10-02 20:17:15 -0300

    base datos

---
+++ src/BaseDeDatos/hotel31.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2023 a las 01:16:45
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hotel31`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `idHabitacion` int(11) NOT NULL,
+  `idTipoHabitacion` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `huesped`
+--
+
+CREATE TABLE `huesped` (
+  `idHuesped` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `domicilio` varchar(100) NOT NULL,
+  `correo` varchar(60) DEFAULT NULL,
+  `celular` bigint(20) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `idReserva` int(11) NOT NULL,
+  `idHabitacion` int(11) NOT NULL,
+  `idHuesped` int(11) NOT NULL,
+  `cantPersonas` int(11) NOT NULL,
+  `fechaEntrada` date NOT NULL,
+  `fechaSalida` date NOT NULL,
+  `importeTotal` double NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipodehabitacion`
+--
+
+CREATE TABLE `tipodehabitacion` (
+  `idTipoHabitacion` int(11) NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `cantCamas` int(11) NOT NULL,
+  `tipoCamas` varchar(20) NOT NULL,
+  `precioNoche` double NOT NULL,
+  `nombreTipo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`idHabitacion`),
+  ADD KEY `idTipoHabitacion` (`idTipoHabitacion`);
+
+--
+-- Indices de la tabla `huesped`
+--
+ALTER TABLE `huesped`
+  ADD PRIMARY KEY (`idHuesped`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`idReserva`),
+  ADD KEY `idHuesped` (`idHuesped`),
+  ADD KEY `idHabitacion` (`idHabitacion`);
+
+--
+-- Indices de la tabla `tipodehabitacion`
+--
+ALTER TABLE `tipodehabitacion`
+  ADD PRIMARY KEY (`idTipoHabitacion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  MODIFY `idHabitacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `huesped`
+--
+ALTER TABLE `huesped`
+  MODIFY `idHuesped` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `idReserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipodehabitacion`
+--
+ALTER TABLE `tipodehabitacion`
+  MODIFY `idTipoHabitacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD CONSTRAINT `habitacion_ibfk_1` FOREIGN KEY (`idTipoHabitacion`) REFERENCES `tipodehabitacion` (`idTipoHabitacion`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`idHuesped`) REFERENCES `huesped` (`idHuesped`),
+  ADD CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`idHabitacion`) REFERENCES `habitacion` (`idHabitacion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
