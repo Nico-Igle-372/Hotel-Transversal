a620a53b09c98fccd6ec6faf10625bc16c4118c7
fgleria <143444867+fgleria@users.noreply.github.com>
2023-10-02 21:00:22 -0300

    idHabitaciones getter&setter

--- src/Entidades/Habitacion.java
+++ src/Entidades/Habitacion.java
@@ -3,21 +3,51 @@ package Entidades;
 
 public class Habitacion {
     
-    private Habitacion habitacion;
+    private int idhabitacion;
     private TipoHabitacion tipoHabitacion;
     private boolean estado;  
 
-    public Habitacion(Habitacion habitacion, TipoHabitacion tipoHabitacion, boolean estado) {
-        this.habitacion = habitacion;
+    public Habitacion() {
+    }
+
+    public Habitacion(int idhabitacion, TipoHabitacion tipoHabitacion, boolean estado) {
+        this.idhabitacion = idhabitacion;
         this.tipoHabitacion = tipoHabitacion;
         this.estado = estado;
     }
 
-  
-
-    public Habitacion() {
+    public int getidhabitacion() {
+        return idhabitacion;
     }
-    
-    
-    
+
+    public void setiDhabitacion(int iDhabitacion) {
+        this.idhabitacion = iDhabitacion;
+    }
+
+    public TipoHabitacion getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "idhabitacion=" + idhabitacion + ", tipoHabitacion=" + tipoHabitacion + ", estado=" + estado + '}';
+    }
+
+  
 }
+   
+    
+    
+
